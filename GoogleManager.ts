d9f9b4936c77078c953ae10c0b5cd9afcaa31e79
PuddingPudding <hwsh70119@gmail.com>
2018-04-10 18:28:10 +0800

    早上先把上回做的google drive範例(javascript，其功能為秀出登入者雲端硬碟上的部份檔案)轉換成了typescript版本，寫到LayaAir裡頭。
    下午的部分則是研究Application Data(google drive API中的一個功能，該功能可以讓我們的APP存資料在登入者的雲端硬碟中，同時不讓那份資料被登入者自由存取)
    然而目前遇到了幾個問題，依照範例上來看的話我們需要用到一個叫做fs(File System)的工具，該工具似乎是node.js的一個功能，需要特別先引入，然而在引入上出現了問題，編輯環境下看沒有錯，但到了瀏覽器那邊看的時候卻會出錯，其原因目前查到是說瀏覽器沒有辦法執行require(引入)功能

--- GoogleManager.ts
+++ GoogleManager.ts
@@ -1,21 +1,26 @@
 /**
 * name 
 */
+
 declare const gapi: any; //聲明未來將會有個叫做gapi的東西，這玩意兒需要在執行後html引入googleAPI後才會產生，編寫階段typescript並不了解
+const fs: any = require("fs");
 module DataManager 
 {
-	
 	export class GoogleManager implements IDataManager
 	{
 		private auth2:any;
+		private drive:any;
 		m_nDefaultExistDays:number; //預設的資料存活時間
 		private static g_googleClient:HTMLMetaElement = document.createElement("meta");
 		private static g_googlePlatform:HTMLScriptElement = document.createElement("script");
 		private static g_googleClientID:string = "224880618061-mg2kf12elebto581h9ssci52kc1v412b.apps.googleusercontent.com";
+		private static g_googleAPIKey:string = "AIzaSyDzdJxuBgZO3ppK9ADZm8NkJ_NrplD6xr0";
+		private static g_driveScope = "https://www.googleapis.com/auth/drive.appdata";
+		private static g_arrDiscoveryDocs:Array<string> = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 
 		constructor() 
 		{
-			gapi.load('auth2', () => 
+			gapi.load('client:auth2', () => 
 			{
 			    this.auth2 = gapi.auth2.init({
 					client_id: GoogleManager.g_googleClientID,
@@ -23,6 +28,13 @@ module DataManager
 					scope: 'profile email'
 				});
 				console.log("成功載入auth並初始化");
+				gapi.client.init({
+					apiKey: GoogleManager.g_googleAPIKey,
+					client_id: GoogleManager.g_googleClientID,
+					discoveryDocs: GoogleManager.g_arrDiscoveryDocs,
+					scope: GoogleManager.g_driveScope
+				});
+				this.drive = gapi.client.drive;
 			});//事前載入gapi中的auth2函式
 
 			// if(!document.head.contains(GoogleManager.g_googleClient) )
@@ -56,6 +68,47 @@ module DataManager
 					console.log('Image URL: ' + profile.getImageUrl());
 					console.log('Email: ' + profile.getEmail());
 					console.log("G Suite domain: " + this.auth2.currentUser.get().getHostedDomain() );
+					
+					gapi.client.drive.files.list({
+						'pageSize': 3,
+						'fields': "nextPageToken, files(id, name)"
+					}).then((response)=>{
+						let files:Array<any> = response.result.files;
+						if (files && files.length > 0)
+						{
+							for (var i = 0; i < files.length; i++)
+							{
+								let file:any = files[i];
+								console.log(file.name + ' (' + file.id + ')');
+							}
+						} else
+						{
+							console.log('No files found.');
+						}
+					});
+
+					let fileMetadata = {
+						'name': 'config.json',
+  						'parents': ['appDataFolder']
+					};
+					let media = {
+						mimeType: 'application/json',
+						body: fs.createReadStream('files/config.json')
+					};
+					this.drive.files.create({
+						resource: fileMetadata,
+						media: media,
+						fields: 'id'
+					}, (err, file) =>{
+						if (err) 
+						{
+							// Handle error
+							console.error(err);
+						} else 
+						{
+							console.log('Folder Id:', file.id);
+						}
+					});
 				}
 			});
 		}
